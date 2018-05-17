2c44238390b79c59b9e06a4122aa7a0bf6a49860
KrzychuW <37503379+KrzychuW@users.noreply.github.com>
2018-05-17 21:07:16 +0200

    Usunięcie zbędnych elementów
    Poprawa pliku .gitignore oraz układu plików w projekcie

---
+++ Core/User/defines.h
@@ -0,0 +1,11 @@
+#ifndef TM_DEFINES_H
+#define TM_DEFINES_H
+
+/* Put your global defines for all libraries here used in your project */
+
+//Default CS pin used
+
+/* Increase SPI5 SCK speed for ILI9341 on STM32F429 discovery board */
+#define TM_SPI5_PRESCALER	SPI_BaudRatePrescaler_2
+#endif
+
